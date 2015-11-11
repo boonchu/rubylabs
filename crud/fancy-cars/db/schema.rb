0b38a92b7e82b47aee11eac99d2d8b2f82110311
Boonchu Ngampairoijpibul <bigchoo@gmail.com>
2015-11-11 08:52:55 -0800

    updated rails db column name

--- crud/fancy-cars/db/schema.rb
+++ crud/fancy-cars/db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907202117) do
+ActiveRecord::Schema.define(version: 20151111164517) do
 
   create_table "cars", force: :cascade do |t|
-    t.string   "company"
+    t.string   "make"
     t.string   "color"
     t.integer  "year"
     t.datetime "created_at",                          null: false
